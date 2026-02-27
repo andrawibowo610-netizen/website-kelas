38ee2f8ad0d76060d26d9d75d3005972b196cd84
andrawibowo610-netizen <andrawibowo610@gmail.com>
2026-02-27 08:36:35 +0700

    Update user.js

--- user.js
+++ user.js
@@ -1,1 +1,23 @@
+// users.js
+// "Database" user (statis, diatur manual)
 
+const USERS = [
+  {
+    username: "dani",
+    password: "danitampan",
+    nama: "ramDani",
+    absen: 27
+  },
+  {
+    username: "dhinggo",
+    password: "dhinggo12",
+    nama: "dhinggo",
+    absen: 3
+  },
+  {
+    username: "jokep",
+    password: "jokep02",
+    nama: "oka pratama",
+    absen: 9
+  }
+];
